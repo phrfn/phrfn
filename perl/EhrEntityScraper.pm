ed37588f0f2d633d16840f773e41394f8b1cbb62
PrinceOf Persia <proper337@gmail.com>
2013-04-06 01:04:17 -0700

    remove debug-mode response logging

--- perl/EhrEntityScraper.pm
+++ perl/EhrEntityScraper.pm
@@ -42,7 +42,7 @@ sub initialize {
     @$self{keys %$args_hash} = values %$args_hash;
     $self->{dbh} = DBI->connect ('dbi:mysql:database=phr', 'root', 'root', {RaiseError => 1, AutoCommit => 1});
 
-    $self->set_canned_config({save_to_canned => defined($args_hash->{save_to_canned})? $args_hash->{save_to_canned} : 1,
+    $self->set_canned_config({save_to_canned => defined($args_hash->{save_to_canned})? $args_hash->{save_to_canned} : 0,
                               read_from_canned => defined($args_hash->{read_from_canned})? $args_hash->{read_from_canned} : 0});
 
     $self->{canned_dir}      = "/var/tmp/canned-ehr-entity-scraper";
